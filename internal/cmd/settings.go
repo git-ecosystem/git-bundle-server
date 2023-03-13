3687877391e18a63f7ca694e507c37570552359b
Victoria Dye <vdye@github.com>
2023-03-13 14:03:56 -0700

    cmd: make run setting key enum values public
    Make the 'settingType' values used to configure behavior in
    'CommandExecutor.Run()' public so that they can be used outside the 'git'
    package (namely, in unit tests added in a later patch).
    
    Signed-off-by: Victoria Dye <vdye@github.com>

--- internal/cmd/settings.go
+++ internal/cmd/settings.go
@@ -9,38 +9,38 @@ import (
 type settingType int
 
 const (
-	stdinKey settingType = iota
-	stdoutKey
-	stderrKey
-	envKey
+	StdinKey settingType = iota
+	StdoutKey
+	StderrKey
+	EnvKey
 )
 
 type Setting utils.KeyValue[settingType, any]
 
 func Stdin(stdin io.Reader) Setting {
 	return Setting{
-		stdinKey,
+		StdinKey,
 		stdin,
 	}
 }
 
 func Stdout(stdout io.Writer) Setting {
 	return Setting{
-		stdoutKey,
+		StdoutKey,
 		stdout,
 	}
 }
 
 func Stderr(stderr io.Writer) Setting {
 	return Setting{
-		stderrKey,
+		StderrKey,
 		stderr,
 	}
 }
 
 func Env(env []string) Setting {
 	return Setting{
-		envKey,
+		EnvKey,
 		env,
 	}
 }
